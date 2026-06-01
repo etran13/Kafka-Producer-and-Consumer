24ba678f9dbf505877b1c06741d9b7a7ae4c8b34
Ellie Tran <154945990+etran13@users.noreply.github.com>
2026-05-31 23:17:27 -0500

    Added print

--- consumer.py
+++ consumer.py
@@ -40,7 +40,7 @@ message_count = 0
 #Get all the messages
 for message in consumer:
     data = json.loads(message.value.decode('utf-8'))
-
+    print(message)
     print(message["random_int"])
     sum += message["random_int"]
     message_count += 1
